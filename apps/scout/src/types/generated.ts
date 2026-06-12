e4c64950d2607321c893be8edd6c6983b238fc56
J.J. Allaire <jj.allaire@gmail.com>
2026-06-12 22:54:58 +0200

    regenerate scout types

--- apps/scout/src/types/generated.ts
+++ apps/scout/src/types/generated.ts
@@ -703,7 +703,7 @@ export interface components {
             max: number;
             /**
              * Min
-             * @default 4
+             * @default 10
              */
             min: number;
             /**
